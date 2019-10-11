3a744bddf8835c1429d1a642e026fe477e0c1991
Daniel Rossi <bovo.96@gmail.com>
2019-10-11 14:28:58 +0200

    altre modifiche

---
+++ validate.hs
@@ -0,0 +1,23 @@
+module Validate where 
+
+    --1)
+toDigits:: Integer -> [Integer]
+toDigits n | n <= 0     = []
+           | otherwise  = toDigits (n`div`10) ++ [n`mod`10]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse (toDigits n)
+--toDigits 1234 == [1,2,3,4]
+
+--2)
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = [x + x*(y`mod`2) | (x,y) <- (reverse (zip (reverse xs) [0..(sum [1 | x <- xs])]))]
+
+--3)
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+
+--4)
+validate :: Integer -> Bool
+validate n = ((sumDigits (doubleEveryOther (toDigits n))) `mod` 10) == 0
