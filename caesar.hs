582bd621fd91a652b43459ca774f9aa3a68d16fb
Daniel Rossi <bovo.96@gmail.com>
2019-10-25 17:57:07 +0200

    es5

--- caesar.hs
+++ caesar.hs
@@ -13,11 +13,37 @@ shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
 
-freq = [('a', 10.85), ('b', 1.05), ('c', 4.3), ('d', 3.39), ('e', 11.49), ('f', 1.01), ('g', 1.65), ('h', 1.43), ('i', 10.18), ('l', 5.7), ('m', 2.87), ('n', 7.02), ('o', 9.97), ('p', 2.96), ('q', 0.45), ('r', 6.19), ('s', 5.48), ('t', 6.97), ('u', 3.16), ('v', 1.75), ('z', 0.85)]
+freqitalian :: [Float]
+freqitalian = [11.74,0.92,4.50,3.73,11.79,0.95,1.64,1.54,11.28,0.01,0.01,6.51,2.51,6.88,9.83,3.05,0.51,6.37,4.98,5.62,3.01,2.10,0.01,0.01,0.01,0.49]
 alpha = ['a'..'z']
 
-foo a b = ( (fromIntegral a) / (fromIntegral b) ) * 100
+percent' a b = ( (fromIntegral a) / (fromIntegral b) ) * 100
 
-currentfreq xs= [foo (sum [1|x<-xs , x == y]) (sum [1|x<-xs]) | y <- alpha]
+currentfreq xs= [percent' (sum [1|x<-xs , x == y]) (sum [1|x<-xs]) | y <- alpha]
 
--- [9.58904109589041e-2,0.0,2.73972602739726e-2,2.28310502283105e-2,6.84931506849315e-2,1.82648401826484e-2,9.1324200913242e-3,1.36986301369863e-2,0.1095890410958904,0.0,0.0,2.28310502283105e-2,2.73972602739726e-2,4.10958904109589e-2,7.30593607305936e-2,2.28310502283105e-2,0.0,7.30593607305936e-2,4.5662100456621e-2,8.67579908675799e-2,3.65296803652968e-2,1.82648401826484e-2,0.0,0.0,0.0,4.5662100456621e-3]
+singleError :: Float -> Float -> Float
+singleError esi osi =  ((osi - esi)^2 ) / esi
+
+error' :: [Float] -> [Float] -> Float
+error' [] [] = 0
+error' (e:es) (o:os) = (singleError e o) + (error' es os)
+
+error'' esi osi = sum [((o - e)^2 ) / e| (o,e) <- zip osi esi]
+
+rotate :: [Float] -> [Float]
+rotate xs = head(reverse xs) : reverse ( tail (reverse xs))  
+
+min' (err1,s1) (err2,s2) | err1 > err2 = (err2,s2)
+                        | otherwise   = (err1,s1)
+
+general :: [Float] -> Int -> (Float,Int)
+general _ 26 = (1000000000,26)
+general xs s = min' ((error'' freqitalian xs),s) (general (rotate xs) (s+1))
+
+example :: String
+example = encode 1 "Nella crittoanalisi, l'analisi delle frequenze lo studio della frequenza di utilizzo delle lettere o gruppi di lettere in un testo cifrato. Questo metodo è utilizzato per violare i cifrari classici. Le indagini quantitative sui testi si servono spesso di qualche forma di analisi delle frequenze.Possono essere interessanti le analisi delle frequenze di caratteri, di parole, di gruppi di parole che si possono assegnare a lemmi o significati definiti; queste analisi possono riguardare un singolo testo (da un frammento epigrafico, a un'opera come la Divina commedia), un intero corpus letterario o un opportuno campione di un linguaggio specialistico o di un'intera lingua."
+
+example1 :: String
+example1 = encode 10 "Importanti furono anche i contatti con il gruppo degli amici bresciani, aperti alle influenze francesi e rivoluzionarie, e con Melchiorre Cesarotti, traduttore dei Canti di Ossian, per il quale Foscolo cominciò a nutrire una notevole ammirazione, giungendo ad intessere rapporti con i letterati che vedevano in lui il loro modello e padre spirituale, e contattandolo personalmente con una missiva del 28 settembre 1795.[19] Il 30 ottobre del 1795 inviò per un parere al Cesarotti, docente presso lo Studio padovano, il manoscritto della tragedia Tieste, di carattere alfieriano e viva di fervori giacobini (rappresentata poi con un certo successo al Teatro Sant'Angelo di Venezia, il 4 gennaio 1797).[20]Foscolo vide subito in Vittorio Alfieri un modello da seguire[21]; egli trasse il suo stile giovanile proprio da lui, e lo decantò in molte opere[22][23]. Foscolo inviò il testo del Tieste, con la dedica[24], alla residenza fiorentina del poeta astigiano. Foscolo preferì non visitare personalmente l'Alfieri, rispettando la sua estrema riservatezza degli ultimi anni, a quanto afferma nell'epistolario e nell'Ortis[25]; pare però che quest'ultimo, anche se non rispose alla lettera del Foscolo, avesse elogiato con alcuni conoscenti lo stile della tragedia, prevedendo il grande avvenire letterario dell'allora giovane ufficiale napoleonico (nonostante l'iniziale disparità di vedute su Napoleone, anche Foscolo poi converrà con Alfieri in un giudizio negativo del generale francese, chiamandolo tiranno[26]) e futuro primo vero poeta-vate dell'Italia risorgimentale. In particolare, avrebbe affermato che quel giovane l'avrebbe superato in quanto a gloria letteraria.[27]"
+
+crack xs = encode (snd (general (currentfreq xs) 0)) xs
