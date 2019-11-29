389e011648a393d54492f53521d3790cb43376e7
Daniel Rossi <bovo.96@gmail.com>
2019-11-29 18:42:11 +0100

    applicative functors

--- es/example.hs
+++ es/example.hs
@@ -2,6 +2,8 @@ data Ciao a = Ciau a | Ciauz (Ciao a) a (Ciao a) deriving Show
 
 data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving Show
 
+data Boia a = End a | El a (Boia a) deriving Show
+
 sumtree :: Num a => Tree a -> a
 sumtree (Leaf x) = x
 sumtree (Node l n r) = (sumtree l) + n + (sumtree r)
@@ -12,7 +14,7 @@ tree = Node (Leaf 1) 2 (Leaf 2)
 ciauz :: Num a=> Ciao [a]
 ciauz = Ciauz (Ciau [1,10]) [1,10] (Ciau [1,10])
 
-ueee :: Num a => Ciao ([a] -> b)
+ueee :: Num a => Ciao ([a] -> a)
 ueee = Ciauz (Ciau (\x -> sum x )) (\x -> sum x) (Ciau (\x -> sum x))
 
 
@@ -44,3 +46,38 @@ instance Applicative Ciao where
     
     (Ciau f) <*> (Ciau x) = pure (f x)
     (Ciauz fl f fr) <*> (Ciauz l n r) = Ciauz (fl <*> l) (f n) (fr <*> r)
+
+instance Functor Boia where
+    fmap f (End x) = End (f x)
+    fmap f (El x y) = El (f x) (fmap f y)
+
+instance Applicative Boia where
+    pure x =  End x
+
+    (End x) <*> (End y) = End (x y)
+    (End x) <*> (El y ys) =  El (x y) (pure x <*> ys)
+    (El x xs) <*> (El y ys) = El (x y) (xs <*> ys)
+ 
+bojon :: Num a => Boia a
+bojon = El 5 (El 6 (El 7 (End 8)))
+
+bojon1 :: Num a => Boia a
+bojon1 = El 5 (El 6 (El 7 (El 8 (End 9))))
+
+data Test a = Tleaf a | Tnode (Test a) (Test a) deriving Show
+
+instance Functor Test where
+    fmap f (Tleaf x) = Tleaf (f x)
+    fmap f (Tnode l r) = Tnode (fmap f l) (fmap f r)
+
+instance Applicative Test where
+    pure x = Tleaf x
+
+    (Tleaf x) <*> (Tleaf y) = Tleaf (x y)
+    (Tleaf x) <*> (Tnode l r) = Tnode (pure x <*> l) (pure x <*> r)
+    (Tnode l r) <*> (Tnode l' r') = Tnode (l <*> l') (r <*> r')
+
+ 
+test1 :: Num a => Test a
+test1 = Tnode (Tnode (Tleaf (5)) (Tleaf 5)) (Tnode (Tleaf 5) (Tleaf 5))
+
